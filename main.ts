2ca46de2c09f2c2a0f064af041e61cdd13973447
Kvothe Guile <112582839+KvotheGuile@users.noreply.github.com>
2022-10-28 20:52:51 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+music.setTempo(120)
+for (let index = 0; index < 2; index++) {
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Whole))
+}
+for (let index = 0; index < 2; index++) {
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(349, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Double))
+}
